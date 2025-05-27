442efecf78f2eabbce7134e9128f780d2e7e4ad3
Samuel François <samffr13@gmail.com>
2025-05-27 11:18:32 -0300

    peças_xadez_Rainha.c

--- peças_xadez_Rainha.c
+++ peças_xadez_Rainha.c
@@ -3,7 +3,7 @@
 int main (){
 // Mover a Rainha 8 casas para a esquerda
     printf("\n----####  Movendo a Rainha 8 casas para a esquerda:  ####----\n");
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++)    //Usando a estrutura de repetição for para executar o movimento 8 vezes
     {
         printf("Esquerda\n");  //imprimere a direção do movimento
     }
