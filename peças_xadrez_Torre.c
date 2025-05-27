0b73d2bc1f684f6501aee3b1a7cc31ae91c65d93
Samuel François <samffr13@gmail.com>
2025-05-27 11:17:40 -0300

    peças_xadrez_Torre.c

--- peças_xadrez_Torre.c
+++ peças_xadrez_Torre.c
@@ -3,9 +3,9 @@
 int main() {
     // Mover a Torre 5 casas para a direita
     printf("----####  Movendo a Torre 5 casas para a direita:  ####----\n");
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++) //Usando a estrutura de repetição for para executar o movimento 5 vezes
     {
         printf("Direita\n");  //imprimere a direção do movimento
     }
-    return 0;
+return 0;
 }
