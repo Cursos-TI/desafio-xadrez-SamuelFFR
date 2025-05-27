5c336f1c4e6c0b55f342451010dd007d0d2b60c4
Samuel François <samffr13@gmail.com>
2025-05-27 11:18:05 -0300

    peças_xadrez_Bispo.c

--- peças_xadrez_Bispo.c
+++ peças_xadrez_Bispo.c
@@ -3,7 +3,7 @@
 int main (){
 // Mover o Bispo 5 casas para a diagonal cima à direita
     printf("\n----####  Movendo o Bispo 5 casas para a diagonal cima direita:  ####----\n");
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)    //Usando a estrutura de repetição for para executar o movimento 5 vezes
     {
         printf("Cima Direita\n");  //imprimere a direção do movimento
     }
